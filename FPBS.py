4c3099531031a2281aa600d4786f8739313c05d8
Zoe Fisch <zoefisch@umich.edu>
2026-03-21 00:35:09 -0400

    Implemented 3 statistics funcitons

--- FPBS.py
+++ FPBS.py
@@ -10,3 +10,44 @@ Description:
 ===============================================================================
 '''
 import numpy as np
+
+def temporal_median_intensity(S):
+    if S.ndim == 0:
+        raise ValueError("Input spectrogram must not be empty")
+    elif S.ndim != 2:
+        raise ValueError("Input spectrogram must be 2D, given spectrogram is ", S.ndim, " dimensions")
+    mu = np.median(S, axis=1)
+    return mu       
+
+def temporal_variability(S, mu):
+    deviation = np.abs(S - mu[:, np.newaxis])
+    sigma = 1.4826 * np.median(deviation, axis=1)
+    return sigma
+
+# Score P = 1 --> band is persistent
+# Score P = 0 --> band is quiet / transient
+def persistence_score(S, mu, sigma, k=2.0):
+    theta = mu + k * sigma
+    P = np.mean(S > theta[:, None], axis=1)
+    return P
+
+    
+
+
+
+        
+
+
+
+
+# computer persistence scores 
+
+# classify channels 
+
+# build supression mask 
+
+# supress persistent channels 
+
+# reconstruct spectrogram
+
+# evaluation 
